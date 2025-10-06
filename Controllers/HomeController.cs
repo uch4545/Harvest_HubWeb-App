6f24e8c28cf7d26879d12b979e677b5a95b5ac5c
TalhaGoMWD <t.rasheed@gomwd.com>
2025-10-06 17:52:18 +0500

    Revert "add error log table but not in flow so add and also add flow of orders"
    This reverts commit c03f41506853eacb477c71bd2fc60debe879475a.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Harvest_Hub__Web_App_.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Harvest_Hub.Controllers
+namespace Harvest_Hub__Web_App_.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Error(string message)
+        public IActionResult Privacy()
         {
-            ViewBag.ErrorMessage = message ?? "An unexpected error occurred.";
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Harvest_Hub__Web_App_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
